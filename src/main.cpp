bc5c2bc33871c47bb838e8460a12e887e969f9b5
krishpuw <144583732+krishpuw@users.noreply.github.com>
2024-11-14 15:58:10 -0500

    test cases

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,25 @@ int rf = 13;
 int rb = 12;
 
 void setup() {
+    pinMode(left, OUTPUT);
+    pinMode(lf, OUTPUT);
+    pinMode(lb, OUTPUT);
+    pinMode(right, OUTPUT);
+    pinMode(rf, OUTPUT);
+    pinMode(rb, OUTPUT);
+    delay(5000);
+
+    forward(5);
+    reverse(5);
+
+    //square
+    for(int i = 0; i < 4; i++){
+        forward(5);
+        turnRight(90);
+    }
+    turnRight(120);
+
+    circle();
   
 }
 
